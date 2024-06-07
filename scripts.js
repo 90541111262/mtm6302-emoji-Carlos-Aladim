aa33d67eeeecc9bba696cb56b9bf69c43aad147a
90541111262 <133885097+90541111262@users.noreply.github.com>
2024-06-07 17:55:18 -0400

    Update the emojis codes
    Update the emojis codes

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const emojis = [
-        { code: '&#128512', decimal: '128512' },
-        { code: '&#128513', decimal: '128513' },
+        { code: '&#128511', decimal: '128511' },
+        { code: '&#128535', decimal: '128535' },
         { code: '&#128514', decimal: '128514' },
-        { code: '&#128515', decimal: '128515' },
-        { code: '&#128516', decimal: '128516' },
+        { code: '&#128530', decimal: '128530' },
+        { code: '&#128544', decimal: '128544' },
         { code: '&#128517', decimal: '128517' },
         { code: '&#128518', decimal: '128518' },
-        { code: '&#128519', decimal: '128519' },
+        { code: '&#128510', decimal: '128510' },
         { code: '&#128520', decimal: '128520' },
-        { code: '&#128521', decimal: '128521' },
+        { code: '&#128509', decimal: '128509' },
         { code: '&#128522', decimal: '128522' },
         { code: '&#128523', decimal: '128523' },
         { code: '&#128524', decimal: '128524' },
-        { code: '&#128525', decimal: '128525' },
+        { code: '&#128516', decimal: '128516' },
         { code: '&#128526', decimal: '128526' },
-        { code: '&#128527', decimal: '128527' }
+        { code: '&#128550', decimal: '128550' }
   
     ];
     
